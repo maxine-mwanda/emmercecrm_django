8924ce15da72e065461e11a8af61e12ee7fe3760
Maxine <mwandamaxyine@gmail.com>
2025-01-06 15:58:24 +0300

    added leads

---
+++ leads/migrations/002_lead_team.py
@@ -0,0 +1,20 @@
+
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('team', '0001_initial'),
+        ('lead', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='lead',
+            name='team',
+            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, related_name='leads', to='team.team'),
+            preserve_default=False,
+        ),
+    ]
