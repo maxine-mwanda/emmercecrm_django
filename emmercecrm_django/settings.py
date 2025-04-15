3d752df2fefdf836663fb824a6ae2751337a2f4f
Maxine <mwandamaxyine@gmail.com>
2025-04-15 23:51:52 +0300

    configs fixing

--- emmercecrm_django/settings.py
+++ emmercecrm_django/settings.py
@@ -11,18 +11,7 @@ CELERY_RESULT_BACKEND = 'django-db'            # Store results in the database
 CELERY_TIMEZONE = 'UTC'
 
 # Celery Beat settings (optional, for periodic tasks)
-INSTALLED_APPS += [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'leads',
-    'django_celery_beat',
-    'django_celery_results',
-]
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -79,6 +68,12 @@ INSTALLED_APPS += [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    *MIDDLEWARE,
 ]
